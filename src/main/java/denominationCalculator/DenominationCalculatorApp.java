5126a9201dba0e220eec2a8d4a45910d1b323570
Amphilyki <adonia_th@hotmail.com>
2025-03-02 22:50:17 +0100

    chore: project setup

---
+++ src/main/java/denominationCalculator/DenominationCalculatorApp.java
@@ -0,0 +1,14 @@
+package denominationCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenominationCalculatorApp {
+    public DenominationCalculatorApp() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DenominationCalculatorApp.class, args);
+    }
+}
